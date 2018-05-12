e4bd9a8da2c59c5164bcd8cee10936a458ee1daf
1yay1 <jmeintrup@googlemail.com>
2018-05-12 13:09:45 +0200

    Added a naive implementation of RankSelect

---
+++ include/sealib/simplerankselect.h
@@ -0,0 +1,32 @@
+//
+// Created by jmeintrup on 10.05.18.
+//
+
+#ifndef SEA_SIMPLERANKSELECT_H
+#define SEA_SIMPLERANKSELECT_H
+#include <boost/dynamic_bitset.hpp>
+namespace Sealib {
+class SimpleRankSelect {
+ private:
+    boost::dynamic_bitset<> bitset;
+
+ public:
+    /**
+     * Selects the k-th set bit
+     * @param k idx
+     * @return k-th set bit
+     */
+    unsigned long select(unsigned long k) const;
+
+    /**
+     * Rank of the k-th idx
+     * @param k idx
+     * @return rank of k-th idx
+     */
+    unsigned long rank(unsigned long k) const;
+
+    SimpleRankSelect(const boost::dynamic_bitset<> &bitset);
+    SimpleRankSelect();
+};
+}
+#endif //SEA_SIMPLERANKSELECT_H
