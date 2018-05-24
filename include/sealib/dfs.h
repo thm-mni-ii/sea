7b344a8efcf4e3d56cb1d1a531cd13a80e285446
Simon Heuser <simon.heuser@mni.thm.de>
2018-05-24 17:06:50 +0200

    * clean up header, + comments

--- include/sealib/dfs.h
+++ include/sealib/dfs.h
@@ -11,27 +11,26 @@ typedef void (*UserFunc2)(Node*,Node*);
 typedef unsigned int uint;
 class DFS {
 	private:
-
+		/* @param u: vertex (in V) to be processed
+		*/
+		static void process_standard(Graph *g,UserFunc1 preProcess,UserFunc2 preExplore,
+			UserFunc2 postExplore,UserFunc1 postProcess,uint *color,uint u);
+			
 	public:
-		DFS();
-		/* Def. runStandardDFS
-			DFS.p: graph G=(V,E) to iterate over
-			DFS.[pre|post][process|explore]: user-defined functions
+		
+		/* runStandardDFS:
+			Run a standard depth-first search over a graph. <br>
+			EFFICIENCY: O(n+m) time, O(n log n) bits
+			@param g graph G=(V,E) to iterate over
+			@param [pre|post][Process|Explore]: user-defined functions <br>
+				to be executed before/after processing a node/exploring an edge
 		*/
 		static void runStandardDFS(Graph *g,UserFunc1 preprocess,
 			UserFunc2 preexplore,UserFunc2 postexplore,
 			UserFunc1 postprocess);
 		
-		/* Def. process_standard
-			process.u: vertex (in V) to be processed
-		*/
-		static void process_standard(Graph *g,UserFunc1 preProcess,UserFunc2 preExplore,
-			UserFunc2 postExplore,UserFunc1 postProcess,uint *color,uint u);
-		
-		uint getColor(uint u);
-		
-		/* Def. nop 
-			- Do nothing. Will be used in place of a NULL user function.
+		/* nop:
+			Do nothing. Will be used in place of a NULL user function.
 		*/
 		static void nop();
 };		
