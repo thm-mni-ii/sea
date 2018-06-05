f43ee7a68f7acdda26aa79f569974cbeb82278a9
Simon Heuser <simon.heuser@mni.thm.de>
2018-06-05 18:30:38 +0200

    + first small DFS test

--- test/dfs_test.cpp
+++ test/dfs_test.cpp
@@ -25,3 +25,17 @@ TEST(DFSTest, runStd) {
 	}
 	free(nodes);
 }
+TEST(DFSTest,runSmall) {
+  Node *nodes = reinterpret_cast<Node*>(calloc(1, sizeof(Node) * 2 * order));
+	for(unsigned int a = 0; a < order - 2; a++) {
+		nodes[a] = Node(new Adjacency(a + 1), 1U);
+	}
+	nodes[order - 1] = Node(new Adjacency(4), 1U);
+  for(int a=0; a<order; a++) s[a]=static_cast<Node*>(0);
+  sp=0;
+  DFS::runSmallDFS(new Graph(nodes,order),pushN,DFS_NOP_EXPLORE,DFS_NOP_EXPLORE,DFS_NOP_PROCESS);
+  while (sp-->0) {
+		ASSERT_EQ(s[sp-1], nodes+sp);
+	}
+  free(nodes);
+}
