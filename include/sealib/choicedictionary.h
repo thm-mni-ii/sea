c1dc6cea6d3de11b6e302ae339966e2fa027296f
Dennis Appel <dennisappel@mailbox.org>
2018-07-22 01:18:15 +0200

    Updated space-efficient skeleton

--- include/sealib/choicedictionary.h
+++ include/sealib/choicedictionary.h
@@ -5,8 +5,21 @@ namespace Sealib {
 
 class ChoiceDictionary {
  private:
-    unsigned int *A, *B;
-    unsigned int length, pointer;
+    #define BLOCK_SIZE 64
+
+    /**
+     * @param primary Array Structure with a word size of 64bits, where each bit represents a color
+     * @param secondary Long Int(64bit) with each set bit pointing to a word in primary containing a color
+     * @param referanceA contains an array of indicies of refereneB.
+     * @param referenceB array stucture containing indicies of refernceA and stores the bit positions of secondary.
+     */
+    // unsigned int *A, *B;
+    uint64_t *primary, secondary;
+    unsigned long long int length;
+    unsigned int *referenceA, *referenceB;
+    unsigned int pointer;
+
+    void createPrimary();
 
     /**
      * Creates a link between A and B by setting and B[pointer-1]=a and
@@ -15,29 +28,33 @@ class ChoiceDictionary {
      */
     unsigned int makeLink(unsigned int target);
 
+    void updateSecondary(unsigned int blockId);
+
+    void updateReferences(unsigned int updatedBlock);
+
  public:
     /**
      * Creates choice dictionary with given size
      * @param _size Size of choice dictionary
      */
-    explicit ChoiceDictionary(unsigned int _length);
+    explicit ChoiceDictionary(unsigned long long int _length);
 
     /**
      * Inserts a color into the specified index
      * @param index Index to insert into
      */
-    void insert(unsigned int index);
+    void insert(unsigned long long int index);
 
     /**
      * Reads the color of A[index] and returns it
      * @param index Index to read from
      */
-    bool get(unsigned int index);
+    bool get(unsigned long long int index);
 
     /**
      * Returns an index i=B[pointer-1] where A[index]=true
      */
-    unsigned int choice();
+    unsigned long long int choice();
 
     ~ChoiceDictionary();
 };
--- src/choicedictionary.cpp
+++ src/choicedictionary.cpp
@@ -3,37 +3,114 @@
 
 using Sealib::ChoiceDictionary;
 
-ChoiceDictionary::ChoiceDictionary(unsigned int _length) : length(_length) {
-    A = new unsigned int[length];
-    B = new unsigned int[length];
+// 4096 farben max?!
+
+ChoiceDictionary::ChoiceDictionary(unsigned long long int _length) : length(_length)
+{
+    referenceA = new unsigned int[BLOCK_SIZE];
+    referenceB = new unsigned int[BLOCK_SIZE];
     pointer = 0;
+    secondary = 0;
+    createPrimary();
 }
 
-void ChoiceDictionary::insert(unsigned int index) {
-    unsigned int linkedIndex;
-    linkedIndex = makeLink(index);
-    A[index] = linkedIndex;
+void ChoiceDictionary::insert(unsigned long long int index)
+{
+    unsigned int blockId = (unsigned int) index / BLOCK_SIZE;
+    unsigned int innerId = index % BLOCK_SIZE;
+
+    uint64_t value = primary[blockId];
+    uint64_t newColor = 1ULL << (BLOCK_SIZE - innerId);
+
+    primary[blockId] = value | newColor;
+
+    updateSecondary(blockId);
+
+    // unsigned int linkedIndex;
+    // linkedIndex = makeLink(index);
+    // A[index] = linkedIndex;
+    // int test = __builtin_clzl(10);
 }
 
-bool ChoiceDictionary::get(unsigned int index) {
-    unsigned int linkedIndex = A[index];
+bool ChoiceDictionary::get(unsigned long long int index)
+{
+    unsigned int referenceBlock = (unsigned int) index / BLOCK_SIZE;
+    unsigned int linkedBlock = referenceA[referenceBlock];
 
-    if (linkedIndex >= length || B[linkedIndex] != index) return false;
+    if (linkedBlock >= length || referenceB[linkedBlock != referenceBlock])
+        return false;
+
+    uint64_t secondaryBlock = 1ULL << (BLOCK_SIZE - referenceBlock);
+
+    if ((secondary & secondaryBlock) == 0)
+        return false;
+
+    unsigned int innerId = index % BLOCK_SIZE;
+    uint64_t primaryValue = primary[referenceBlock];
+    uint64_t primaryPosition = 1ULL << (BLOCK_SIZE - innerId);
+
+    if ((primaryValue & primaryPosition) == 0)
+        return false;
+
+    // unsigned int linkedIndex = A[index];
+
+    // if (linkedIndex >= length || B[linkedIndex] != index) return false;
     return true;
 }
 
-unsigned int ChoiceDictionary::choice() {
-    return B[pointer - 1];
+unsigned long long int ChoiceDictionary::choice()
+{
+    bool found = false;
+    uint64_t innerIndex = 1ULL;
+    unsigned int shiftCount = 0;
+
+    uint64_t blockId = referenceB[pointer-1];
+    uint64_t primaryValue = primary[blockId];
+
+    while(!found) {
+        if ((primaryValue & innerIndex) != 0){
+            found = true;
+        }
+        else {
+            innerIndex = innerIndex << 1;
+            shiftCount++;
+        }
+    }
+
+    return blockId * BLOCK_SIZE + (BLOCK_SIZE - shiftCount);
+    // return B[pointer - 1];
 }
 
-unsigned int ChoiceDictionary::makeLink(unsigned int target) {
-    unsigned int linkedIndex = pointer;
-    B[linkedIndex] = target;
-    pointer++;
-    return linkedIndex;
+void ChoiceDictionary::createPrimary()
+{
+    unsigned long long int size = length / BLOCK_SIZE;
+    primary = new uint64_t[size];
 }
 
-ChoiceDictionary::~ChoiceDictionary() {
-    delete[] A;
-    delete[] B;
+void ChoiceDictionary::updateSecondary(unsigned int blockId)
+{
+    uint64_t updatedBlock = 1ULL << (BLOCK_SIZE - blockId);
+    secondary = secondary | updatedBlock;
+
+    updateReferences(blockId);
+}
+
+void ChoiceDictionary::updateReferences(unsigned int blockId)
+{
+    referenceA[blockId] = makeLink(blockId);
+}
+
+unsigned int ChoiceDictionary::makeLink(unsigned int target)
+{
+    referenceB[pointer] = target;
+    if (pointer < target)
+        pointer++;
+    return pointer;
+}
+
+ChoiceDictionary::~ChoiceDictionary()
+{
+    delete[] referenceA;
+    delete[] referenceB;
+    delete[] primary;
 }
