7289936d8b7c7396fee621f953a020cb34028ac8
1yay1 <jmeintrup@googlemail.com>
2018-07-23 14:36:02 +0200

    Made bitset generic. Now able to use blocks of following types: unsigned long unsigned int unsigned char

--- test/bitset_test.cpp
+++ test/bitset_test.cpp
@@ -4,21 +4,23 @@
 using Sealib::Bitset;
 
 TEST(BitsetTest, BitsetFunctionality) {
-    Bitset bs;
+    typedef unsigned long block;
+
+    Bitset<block> bs;
     std::cout << bs.size() << std::endl;
     std::cout << bs.blocks() << std::endl;
     ASSERT_EQ(bs.size(), 0);
     ASSERT_EQ(bs.blocks(), 0);
 
-    bs = Bitset(1);
+    bs = Bitset<block> (1);
     ASSERT_EQ(bs.size(), 1);
     ASSERT_EQ(bs.blocks(), 1);
 
-    bs = Bitset(64);
+    bs = Bitset<block>(64);
     ASSERT_EQ(bs.size(), 64);
     ASSERT_EQ(bs.blocks(), 1);
 
-    bs = Bitset(65);
+    bs = Bitset<block>(65);
     ASSERT_EQ(bs.size(), 65);
     ASSERT_EQ(bs.blocks(), 2);
 
@@ -28,7 +30,7 @@ TEST(BitsetTest, BitsetFunctionality) {
         bs[i] = 1;
     }
 
-    Bitset bs_(65);
+    Bitset<block> bs_(65);
     for(unsigned int i = 0; i < bs.size(); i++) {
         ASSERT_EQ(bs[i], 1);
         ASSERT_EQ(bs.get(i), bs[i]);
@@ -64,13 +66,10 @@ TEST(BitsetTest, BitsetFunctionality) {
     bs.setBlock(1,0);
     ASSERT_EQ(bs[bs.size() - 1], 0);
 
-    bs.setBlock(0, (unsigned long) - 1);
+    bs.setBlock(0, (block) - 1);
     for(unsigned int i = 0; i < bs.size() - 1; i++) {
         ASSERT_EQ(bs[i], 1);
     }
 
-    ASSERT_EQ(bs.getBlock(0), (unsigned long) - 1);
-
-    Bitset bsCpy(bs);
-    ASSERT_EQ(bsCpy, bs);
+    ASSERT_EQ(bs.getBlock(0), (block) - 1);
 }
