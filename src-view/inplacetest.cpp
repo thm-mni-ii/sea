9b2005797d9489be35d2db5cd2759820f91c6b21
simonschniedenharn <simon.schniedenharn@protonmail.com>
2018-07-26 19:32:52 +0200

    fix swapped beginpointer to standard representation

--- src-view/inplacetest.cpp
+++ src-view/inplacetest.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 #include "sealib/graphrepresentations.h"
 #include "sealib/graph.h"
 #include "sealib/graphcreator.h"
@@ -15,20 +16,44 @@ void printGraph(unsigned int* a){
 	std::cout << std::endl;
 }
 int main(){
-	unsigned int n = 5;
-	float p = 0.5;	
-	unsigned int *a = Graphrepresentations::generateStandardGraph(n,p);
-	std::cout << "standard representation:" << std::endl;
-	printGraph(a);
+std::srand(static_cast<unsigned int>(std::time(nullptr)));
+  unsigned int n = 50;
+  float p = 0.5;	
+  unsigned int error = 0;
+  for(unsigned int i = 0; i < 1000; ++i){
+	 unsigned int *a = Graphrepresentations::generateStandardGraph(n,p);
+  	 unsigned int size = a[0] + a[a[0] + 1] + 2;
+  	 unsigned int *b = new unsigned int[size];
+		for(unsigned int j = 0; j < size; j++){
+		  b[j] = a[j];
+		}
+	//std::cout << "standard representation:" << std::endl;
+	//printGraph(a);
 	Graphrepresentations::standardToBeginpointer(a);
-	std::cout << "beginpointer representation:" << std::endl;
-	printGraph(a);
+	//std::cout << "beginpointer representation:" << std::endl;
+	//printGraph(a);
 	Graphrepresentations::swapRepresentation(a);
-	std::cout << "swapped beginpointer representation:" << std::endl;
-	printGraph(a);
+	//std::cout << "swapped beginpointer representation:" << std::endl;
+	//printGraph(a);
 	Graphrepresentations::swappedBeginpointerToStandard(a);
-	std::cout << "standard representation:" << std::endl;
-	printGraph(a);
+	//std::cout << "a standard representation:" << std::endl;
+	//printGraph(a);
+   bool equal = true;
+   for(unsigned int j = 0; j < size; j++){
+	  if(b[j] != a[j]){
+		 equal = false;
+		 }
+	}
+	if(!equal){
+//	std::cout << "b standard representation:" << std::endl;
+//	printGraph(b);
+//	std::cout << std::endl;
+	  error++;
+	}
+  delete a;
+  delete b;
+  }
+  std::cout << error << std::endl;
 	return 0;
 }
 
