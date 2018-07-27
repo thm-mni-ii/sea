f21fe5c90e8ad19baee82081e74759ab3ac71a4e
simonschniedenharn <simon.schniedenharn@protonmail.com>
2018-07-27 17:53:50 +0200

    add runtime measuring class for graph search algorithms

--- src-view/inplacetest.cpp
+++ src-view/inplacetest.cpp
@@ -4,29 +4,32 @@
 #include "sealib/graph.h"
 #include "sealib/graphcreator.h"
 #include "sealib/dfs.h"
+#include "sealib/runtimemeasurer.h"
 
-void printGraph(unsigned int* a){
-	for(uint i = 0; i < a[0] + a[a[0]+1] + 2; ++i){
-		std::cout << i << "\t";
-	}
-	std::cout << std::endl;
-	for(uint i = 0; i < a[0] + a[a[0]+1] + 2; ++i){
-		std::cout << a[i] << "\t";
-	}
-	std::cout << std::endl;
-}
 int main(){
 	std::srand(static_cast<unsigned int>(std::time(nullptr)));
-  unsigned int n = 30000;
-  float p = 0.5;	
-	 unsigned int *a = Graphrepresentations::generateStandardGraph(n,p);
-	 std::cout << "standard representation:" << std::endl;
-	 printGraph(a);
-	 Graphrepresentations::standardToBeginpointer(a);
-	 Graphrepresentations::swapRepresentation(a);
-	 printGraph(a);
-	 DFS::runLinearTimeInplaceDFS(a,3);
-  delete a;
+  unsigned int n;
+	unsigned int w;
+	RuntimeMeasurer runner;
+	n = 6000;
+	w = n*(n-1) * 0.5;
+	runner.runFunction([n,w](){Graphrepresentations::generateStandardGraph(n,0.5f);},n,w);
+	n = 5000;
+	w = n*(n-1) * 0.5;
+	runner.runFunction([n,w](){Graphrepresentations::generateStandardGraph(n,0.5f);},n,w);
+	n = 4000;
+	w = n*(n-1) * 0.5;
+	runner.runFunction([n,w](){Graphrepresentations::generateStandardGraph(n,0.5f);},n,w);
+	n = 3000;
+	w = n*(n-1) * 0.5;
+	runner.runFunction([n,w](){Graphrepresentations::generateStandardGraph(n,0.5f);},n,w);
+	n = 2000;
+	w = n*(n-1) * 0.5;
+	runner.runFunction([n,w](){Graphrepresentations::generateStandardGraph(n,0.5f);},n,w);
+	n = 1000;
+	w = n*(n-1) * 0.5;
+	runner.runFunction([n,w](){Graphrepresentations::generateStandardGraph(n,0.5f);},n,w);
+	runner.printResults();
 	return 0;
 }
 
