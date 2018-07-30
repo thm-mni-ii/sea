51b1a7386dc6249a8def6b61ad2509c4b908fefa
simonschniedenharn <simon.schniedenharn@protonmail.com>
2018-07-30 13:11:27 +0200

    add comments for runtimetest class

--- include/sealib/runtimetest.h
+++ include/sealib/runtimetest.h
@@ -6,36 +6,54 @@
 #include <vector>
 #include <string>
 #include <tuple>
-
-class RuntimeMeasurer{
+/**
+ * This class is used to perform runtime tests on
+ * graph search algorithms. The results can either
+ * be printed into the standard output or into a 
+ * CSV (comma seperated values) file.
+ */
+class RuntimeTest{
 	private:
 		std::vector<std::tuple<unsigned int,unsigned int> > parameters;
-		//runtimes in seconds
-		std::vector<double> runtimes;
-
+		std::vector<double> runtimes; ///<runtime in seconds
 	public:
-		template<typename Func>
-			void runFunction(Func funktor,unsigned int nodes, unsigned int edges);
+		/**
+		 * Runs a test with a given function and saves the results
+		 * @param testfunction a template type to be used with a lambda to specify
+		 * wich functions runtime shall be tested
+		 * @param order order of the tested graph used for running the test
+		 * @param size size of the tested graph used for running the test
+		 */
+		template<typename Testfunction>
+		void runTest(Testfunction testfunction,unsigned int order, unsigned int size);
+		/**
+		 * Prints the testresults to standard output
+		 */
 		void printResults();	
-		void printToCSV(std::string path);
+		/**
+		 * Saves the testresults
+		 * @params filepath path to the file in wich the results are to be saved
+		 * the file will be truncated
+		 */
+		void saveCSV(std::string filepath);
 };
 
-template<typename Func>
-void RuntimeMeasurer::runFunction(Func funktor, unsigned int nodes, unsigned int edges){
-	std::tuple<unsigned int,unsigned int> funcParameters(nodes,edges);
+template<typename Testfunction>
+void RuntimeTest::runTest(Testfunction testfunction, unsigned int order, unsigned int size){
+	std::tuple<unsigned int,unsigned int> funcParameters(order,size);
 	parameters.push_back(funcParameters);
 	struct tms before;
 	struct tms after; 
-	std::cout << "Running test: "<< parameters.size() << " n: "<< nodes << " w: "<< edges << std::endl;
+	std::cout << "Running test: "<< parameters.size() << " n: "<< order << " w: "<< size << std::endl;
 	times(&before);
-	funktor();
+	testfunction();
 	times(&after);
 	double elapsedTime = ((double)((after.tms_utime + after.tms_stime)
 			- (before.tms_utime + before.tms_stime))) / sysconf(_SC_CLK_TCK);
 	runtimes.push_back(elapsedTime); 
 } 
 
-void RuntimeMeasurer::printResults(){
+void RuntimeTest::printResults(){
 	for(unsigned int i = 0; i < parameters.size(); ++i){
 		std::cout <<"Nodes: " << std::get<0>(parameters[i]) << std::endl;
 		std::cout <<"Edges: " << std::get<1>(parameters[i]) << std::endl;	
@@ -43,9 +61,9 @@ void RuntimeMeasurer::printResults(){
 	}
 }
 
-void RuntimeMeasurer::printToCSV(std::string path){
+void RuntimeTest::saveCSV(std::string filepath){
 	std::ofstream output;
-	output.open(path,std::ofstream::out | std::ofstream::trunc);
+	output.open(filepath,std::ofstream::out | std::ofstream::trunc);
 	output << "order,size,runtime" << std::endl;
 	for(unsigned int i = 0; i < parameters.size(); ++i){
 		output<<std::get<0>(parameters[i])<<",";
