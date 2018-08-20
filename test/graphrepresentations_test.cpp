fc49f9bb694e9a4f670023a1b73fbb193d773587
Andrej Sajenko <Andrej.Sajenko@mni.thm.de>
2018-08-20 16:51:29 +0200

    begin pointer tests

--- test/graphrepresentations_test.cpp
+++ test/graphrepresentations_test.cpp
@@ -35,3 +35,17 @@ TEST(GraphTest, graph_generation) {
 
     delete g;
 }
+
+TEST(GraphTest, graph_beginpointer) {
+    uint *A = new uint[16] {5,  7, 9, 11, 13, 15,  9,  2, 4,  3, 4,  1, 2,  2, 3,  3};
+
+    Graphrepresentations::standardToBeginpointer(A);
+
+    ASSERT_EQ(A[0], 5);
+
+    ASSERT_EQ(A[7], 9);
+    ASSERT_EQ(A[9], 11);
+    ASSERT_EQ(A[11], 7);
+    ASSERT_EQ(A[13], 9);
+    ASSERT_EQ(A[15], 11);
+}
