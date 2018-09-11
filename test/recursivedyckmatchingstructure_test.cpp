a123cb308ec8d59a5848ee60ac163e9e36accef5
Johannes Meintrup <jmeintrup@googlemail.com>
2018-09-11 12:13:53 +0200

    added tests for recursive dyckmatching structure

--- test/recursivedyckmatchingstructure_test.cpp
+++ test/recursivedyckmatchingstructure_test.cpp
@@ -2,15 +2,22 @@
 #include <include/sealib/recursivedyckmatchingstructure.h>
 #include <include/sealib/dyckwordlexicon.h>
 
-/*TEST(RecursiveDyckMatchingStructureTest, testAllWordsLength10) {
-    Sealib::DyckWordLexicon lex(10);
-    for (const Sealib::Bitset<unsigned char> &word : lex.getLexicon()) {
-        Sealib::RecursiveDyckMatchingStructure d(word);
-        for (unsigned int i = 0; i < 10; i++) {
-            ASSERT_NE(d.getMatch(i), i);
+TEST(RecursiveDyckMatchingStructureTest, testAllWordsLength10) {
+    for (unsigned int i = 2; i < 28; i += 2) {
+        Sealib::DyckWordLexicon lex(i);
+        for (const Sealib::Bitset<unsigned char> &word : lex.getLexicon()) {
+            Sealib::DyckMatchingStructure d(word);
+            Sealib::RecursiveDyckMatchingStructure dRec(word);
+            for (unsigned int j = 0; j < i; j++) {
+                unsigned long dMatch = d.getMatch(j);
+                unsigned long dRecMatch = dRec.getMatch(j);
+                ASSERT_NE(dMatch, j);
+                ASSERT_NE(dRecMatch, j);
+                ASSERT_EQ(dMatch, dRecMatch);
+            }
         }
     }
-}*/
+}
 
 TEST(RecursiveDyckMatchingStructureTest, testWords) {
     Sealib::Bitset<unsigned char> word(16);
@@ -44,6 +51,7 @@ TEST(RecursiveDyckMatchingStructureTest, testWords) {
         word2[i] = 1;
     }
     Sealib::RecursiveDyckMatchingStructure d2(word2);
+    Sealib::DyckMatchingStructure d2simple(word2);
 
     ASSERT_EQ(d2.getMatch(0), 9);
     ASSERT_EQ(d2.getMatch(1), 8);
@@ -56,4 +64,16 @@ TEST(RecursiveDyckMatchingStructureTest, testWords) {
     ASSERT_EQ(d2.getMatch(7), 2);
     ASSERT_EQ(d2.getMatch(6), 3);
     ASSERT_EQ(d2.getMatch(5), 4);
+
+    ASSERT_EQ(d2.getMatch(0), d2simple.getMatch(0));
+    ASSERT_EQ(d2.getMatch(1), d2simple.getMatch(1));
+    ASSERT_EQ(d2.getMatch(2), d2simple.getMatch(2));
+    ASSERT_EQ(d2.getMatch(3), d2simple.getMatch(3));
+    ASSERT_EQ(d2.getMatch(4), d2simple.getMatch(4));
+
+    ASSERT_EQ(d2.getMatch(5), d2simple.getMatch(5));
+    ASSERT_EQ(d2.getMatch(6), d2simple.getMatch(6));
+    ASSERT_EQ(d2.getMatch(7), d2simple.getMatch(7));
+    ASSERT_EQ(d2.getMatch(8), d2simple.getMatch(8));
+    ASSERT_EQ(d2.getMatch(9), d2simple.getMatch(9));
 }
