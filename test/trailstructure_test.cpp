188bf63c26330282962aa5652d2fa4246979d1c5
Johannes Meintrup <jmeintrup@googlemail.com>
2018-09-13 14:14:13 +0200

    added tests for trailstructure

--- test/trailstructure_test.cpp
+++ test/trailstructure_test.cpp
@@ -1,22 +1,22 @@
 #include <gtest/gtest.h>
-#include <sealib/simpletrailstructure.h>
+#include <sealib/trailstructure.h>
 
-using Sealib::SimpleTrailStructure;
+using Sealib::TrailStructure;
 
-TEST(SimpleTrailStructureTest, enter) {
-    SimpleTrailStructure ts = SimpleTrailStructure(5);
+TEST(TrailStructureTest, enter) {
+    TrailStructure ts = TrailStructure(5);
 
     ASSERT_EQ(ts.enter(2), 3);
     ASSERT_EQ(ts.enter(4), 0);
     ASSERT_EQ(ts.enter(1), (unsigned int) -1);  // last element
 }
 
-TEST(SimpleTrailStructureTest, leave) {
-    SimpleTrailStructure ts = SimpleTrailStructure(5);
+TEST(TrailStructureTest, leave) {
+    TrailStructure ts = TrailStructure(5);
 
     ASSERT_EQ(ts.leave(), 0);
 
-    ts = SimpleTrailStructure(5);
+    ts = TrailStructure(5);
 
     ASSERT_EQ(ts.leave(), 0);
     ASSERT_EQ(ts.leave(), 1);
@@ -25,8 +25,8 @@ TEST(SimpleTrailStructureTest, leave) {
     ASSERT_EQ(ts.leave(), 4);
 }
 
-TEST(SimpleTrailStructureTest, matches) {
-    SimpleTrailStructure ts = SimpleTrailStructure(5);
+TEST(TrailStructureTest, matches) {
+    TrailStructure ts = TrailStructure(5);
 
     ts.enter(2);
     ts.enter(4);
@@ -39,24 +39,29 @@ TEST(SimpleTrailStructureTest, matches) {
     ASSERT_EQ(ts.getMatched(4), 0);
 }
 
-TEST(SimpleTrailStructureTest, marry) {
-    SimpleTrailStructure ts = SimpleTrailStructure(5);
+TEST(TrailStructureTest, marry) {
+    TrailStructure ts = TrailStructure(5);
 
     ts.enter(2);
     ts.enter(4);
     ts.enter(1);
 
+    ASSERT_EQ(ts.getMatched(0), 4);
+    ASSERT_EQ(ts.getMatched(4), 0);
+    ASSERT_EQ(ts.getMatched(1), 1);
     ts.marry(0, 1);
+    ASSERT_EQ(ts.getMatched(0), 1);
+    ASSERT_EQ(ts.getMatched(1), 0);
+    ASSERT_EQ(ts.getMatched(4), 4);
 
     ASSERT_EQ(ts.getMatched(0), 1);
     ASSERT_EQ(ts.getMatched(1), 0);  // had no match before
     ASSERT_EQ(ts.getMatched(2), 3);
     ASSERT_EQ(ts.getMatched(3), 2);
-    ASSERT_EQ(ts.getMatched(4), 4);  // unmatched now
 }
 
-TEST(SimpleTrailStructureTest, enterLeaveCombination) {
-    SimpleTrailStructure ts = SimpleTrailStructure(5);
+TEST(TrailStructureTest, enterLeaveCombination) {
+    TrailStructure ts = TrailStructure(5);
 
     ASSERT_EQ(ts.enter(0), 1);
     ASSERT_EQ(ts.enter(4), 2);
