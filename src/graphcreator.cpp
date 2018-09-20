c147f57825299abbcd38e57471956019241cecd2
Johannes Meintrup <jmeintrup@googlemail.com>
2018-09-20 09:08:57 +0200

    fixed linter errors, fixed merge conflicts

--- src/graphcreator.cpp
+++ src/graphcreator.cpp
@@ -1,6 +1,10 @@
+<<<<<<< HEAD
 #include "sealib/graphcreator.h"
 #include <stdlib.h>
 #include <iostream>
+=======
+#include <sealib/graphcreator.h>
+>>>>>>> fixed linter errors, fixed merge conflicts
 #include <limits>
 #include <set>
 
@@ -11,13 +15,14 @@ Basicgraph *GraphCreator::createGraphFromAdjacencyMatrix(
     unsigned int **adjMatrix, unsigned int order) {
   Node *nodes = static_cast<Node *>(malloc(sizeof(Node) * order));
 
-  for (unsigned int i = 0; i < order; i++) {
-    unsigned int deg = 0;
+    for (unsigned int i = 0; i < order; i++) {
+        unsigned int deg = 0;
 
-    for (unsigned int j = 0; j < order; j++) {
-      deg += adjMatrix[i][j];
-    }
+        for (unsigned int j = 0; j < order; j++) {
+            deg += adjMatrix[i][j];
+        }
 
+<<<<<<< HEAD
     Adjacency *adj = static_cast<Adjacency *>(malloc(sizeof(Adjacency) * deg));
 
     int idx = 0;
@@ -47,16 +52,46 @@ Basicgraph *GraphCreator::createGraphFromAdjacencyMatrix(
             adj_arr[j].crossIndex = _j;
             break;
           }
-        }
-      }
-    }
-  }
+=======
+        std::vector<Adjacency> adj(deg);
 
-  return new Basicgraph(nodes, order);
+        int idx = 0;
+        for (unsigned int j = 0; j < order; j++) {
+            for (unsigned int k = 0; k < adjMatrix[i][j]; k++) {
+                adj[idx++] = Adjacency(j);
+            }
+        }
+        nodes.emplace_back(adj);
+    }
+    for (unsigned int i = 0; i < order; i++) {
+        const unsigned int deg = nodes[i].getDegree();
+        const std::vector<Adjacency> &adj_arr = nodes[i].getAdj();
+
+        for (unsigned int j = 0; j < deg; j++) {
+            if (adj_arr[j].crossIndex == std::numeric_limits<unsigned int>::max()) {
+                unsigned int v = adj_arr[j].vertex;
+                const std::vector<Adjacency> &_adj_arr = nodes[v].getAdj();
+                const unsigned int _deg = nodes[v].getDegree();
+
+                for (unsigned int _j = 0; _j < _deg; _j++) {
+                    if (_adj_arr[_j].crossIndex == std::numeric_limits<unsigned int>::max() &&
+                        _adj_arr[_j].vertex == i) {
+                        nodes[v].setCrossIndex(_j, j);
+                        nodes[i].setCrossIndex(j, _j);
+                        break;
+                    }
+                }
+            }
+>>>>>>> fixed linter errors, fixed merge conflicts
+        }
+    }
+
+    return Basicgraph(nodes);
 }
 
 static std::random_device rng;
 
+<<<<<<< HEAD
 Basicgraph *GraphCreator::createRandomFixed(unsigned int order,
                                             unsigned int degreePerNode) {
   std::uniform_int_distribution<unsigned int> rnd(0, order - 1);
@@ -143,4 +178,33 @@ std::unique_ptr<Sealib::BasicGraph> Sealib::GraphCreator::generateRandomBipartit
     }
 
     return graph;
+=======
+Sealib::Basicgraph *Sealib::GraphCreator::createRandomFixed(unsigned int order,
+                                                            unsigned int degreePerNode) {
+    std::uniform_int_distribution<unsigned int> rnd(0, order - 1);
+    std::vector<Node> n(order);
+    for (unsigned int a = 0; a < order; a++) {
+        unsigned int ai = degreePerNode;
+        std::vector<Adjacency> ad(ai);
+        for (unsigned int b = 0; b < ai; b++) {
+            ad[b] = Adjacency(rnd(rng));
+        }
+        n[a] = Node(ad);
+    }
+    return new Basicgraph(n);
+}
+
+Sealib::Basicgraph *Sealib::GraphCreator::createRandomGenerated(unsigned int order) {
+    std::vector<Node> n(order);
+    std::uniform_int_distribution<unsigned int> rnd(0, order - 1);
+    for (unsigned int a = 0; a < order; a++) {
+        unsigned int deg = rnd(rng);
+        std::vector<Adjacency> ad(ai);
+        for (unsigned int b = 0; b < deg; b++) {
+            ad[b] = Adjacency(rnd(rng));
+        }
+        n[a] = Node(ad);
+    }
+    return new Basicgraph(n);
+>>>>>>> fixed linter errors, fixed merge conflicts
 }
