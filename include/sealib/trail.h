3185ce4c18530e01433b66c8c02079839746b246
Johannes Meintrup <jmeintrup@googlemail.com>
2018-09-23 20:19:33 +0200

    added EulerTrail class to replace hierholzer algorithm in GraphAlgorithms. After constructing the trail via hierholzer the user can use an iterator to iterate over the created trail space efficient version: the trail is space efficient

--- include/sealib/trail.h
+++ include/sealib/trail.h
@@ -7,55 +7,66 @@
 #include <sealib/trailstructure.h>
 #include <ostream>
 #include <vector>
-
-using rankselect_t = Sealib::RankSelect;
-using std::vector;
-using graph_t = Sealib::BasicGraph;
-using graphptr_t = std::shared_ptr<graph_t>;
-using bitset_t = Sealib::Bitset<unsigned char>;
+#include <tuple>
 
 namespace Sealib {
 template<class TrailStructureType>
 class EulerTrail {
+    typedef typename Sealib::RankSelect rankselect_t;
+    typedef typename Sealib::BasicGraph graph_t;
+    typedef typename std::shared_ptr<graph_t> graphptr_t;
+    typedef typename Sealib::Bitset<unsigned char> bitset_t;
+    typedef typename std::vector<TrailStructureType> array_t;
+
  private:
     graphptr_t graph;
-    vector<TrailStructureType> trail;
+    array_t trail;
     rankselect_t trailStarts;
 
     unsigned int findStartingNode();
+    array_t initializeTrail();
     bitset_t findTrailStarts();
+
+    class iterator {
+     public:
+        explicit iterator(const EulerTrail<TrailStructureType> &Container, unsigned int index = 1);
+        std::tuple<unsigned long, bool> operator*() const;
+        iterator &operator++();
+        iterator &operator++(int);
+        bool operator!=(const iterator &) const;
+     private:
+        unsigned int nIndex;
+        unsigned int mIndex;
+        unsigned int arc;
+        bool ending;
+        const EulerTrail<TrailStructureType> &eulerTrail;
+    };
 
  public:
     explicit EulerTrail(const graphptr_t &graph);
 
-    vector<TrailStructureType> initializeTrail();
-
     friend std::ostream &operator<<(std::ostream &os,
                                     const EulerTrail<TrailStructureType> &eulerTrail) {
-        unsigned long maxRank = eulerTrail.trailStarts.rank(eulerTrail.trailStarts.size());
-        for (unsigned int i = 1; i <= maxRank; i++) {
-            unsigned int v0 = static_cast<unsigned int>(eulerTrail.trailStarts.select(i)) - 1;
-            unsigned int arc = eulerTrail.trail[v0].getStartingArc();
-            if (arc != (unsigned int) -1) {
-                os << "T" << i << ": " << v0 << " -> ";
-                unsigned int u = v0;
-                while (arc != (unsigned int) -1) {
-                    unsigned int uCross = eulerTrail.graph->getNode(u).getAdj()[arc].crossIndex;
-                    u = eulerTrail.graph->getNode(u).getAdj()[arc].vertex;
-
-                    os << u;
-                    arc = eulerTrail.trail[u].getMatched(uCross);
-                    if (arc == uCross) {
-                        arc = (unsigned int) -1;
-                        os << std::endl;
-                    } else {
-                        os << " -> ";
-                    }
+        unsigned int tourNum = 1;
+        bool newTour = true;
+        for (auto v0 : eulerTrail) {
+            bool ending = std::get<1>(v0);
+            if (!ending) {
+                if (newTour) {
+                    newTour = false;
+                    os << "T" << tourNum++ << ": ";
                 }
+                os << std::get<0>(v0) << " -> ";
+            }
+            if (ending) {
+                newTour = true;
+                os << std::get<0>(v0) << std::endl;
             }
         }
         return os;
     }
+    iterator begin() const;
+    iterator end() const;
 };
 }  // namespace Sealib
 #endif  // SEALIB_TRAIL_H_
