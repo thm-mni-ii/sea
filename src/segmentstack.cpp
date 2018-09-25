20a7328cc2e278a19c96c37231907e4bd432ed04
Simon Heuser <simon.heuser@mni.thm.de>
2018-09-25 18:58:05 +0200

    add functionality to the expanded stack (+ tests), improve restoration

--- src/segmentstack.cpp
+++ src/segmentstack.cpp
@@ -1,4 +1,5 @@
 #include "src/segmentstack.h"
+#include <iostream>
 
 using Sealib::SegmentStack;
 using Sealib::BasicSegmentStack;
@@ -34,7 +35,7 @@ int SegmentStack::pop(Pair *r) {
   return 0;
 }
 
-bool SegmentStack::empty() {
+bool SegmentStack::isEmpty() {
   bool r;
   r = lp == 0 && hp == 0 && tp == 0;
   return r;
@@ -46,7 +47,6 @@ BasicSegmentStack::BasicSegmentStack(unsigned segmentSize)
     : SegmentStack(segmentSize) {}
 
 int BasicSegmentStack::push(Pair u) {
-  // if(t) table[u.head()]=tp;
   if (lp < q) {
     low[lp++] = u;
   } else if (hp < q) {
@@ -95,11 +95,15 @@ bool BasicSegmentStack::isAligned() {
 
 //  -- EXTENDED --
 
-ExtendedSegmentStack::ExtendedSegmentStack(uint size, unsigned segmentSize)
+ExtendedSegmentStack::ExtendedSegmentStack(uint size, unsigned segmentSize,
+                                           Graph *g, CompactArray *c)
     : SegmentStack(segmentSize),
       trailers(new Pair[size / q + 1]),
       l(static_cast<unsigned>(log(size))),
-      table(new CompactArray(size, q, l)) {}
+      table(new CompactArray(size, q, l)),
+      edges(new CompactArray(size, q, l)),
+      graph(g),
+      color(c) {}
 
 ExtendedSegmentStack::~ExtendedSegmentStack() {
   delete[] trailers;
@@ -120,7 +124,49 @@ int ExtendedSegmentStack::push(Pair u) {
     hp = 0;
     high[hp++] = u;
   }
+  uint uv = u.head(), uk = u.tail();
+  table->insert(uv, tp);  // ?
+  if (graph->getNodeDegree(uv) > graph->getOrder() / q) {
+    // to do: add big vertex storage in trailers
+  } else {
+    double g = ceil(graph->getNodeDegree(uv) / l);
+    unsigned f = static_cast<unsigned>(floor((uk - 1) / g));
+    edges->insert(uv, f);
+  }
+  std::cout << "lp=" << lp << ", hp=" << hp << ", tp=" << tp << "\n";
   return 0;
 }
 
-bool ExtendedSegmentStack::isAligned() { return true; }
+bool ExtendedSegmentStack::isInTopSegment(uint u) {
+  bool r;
+  r = table->get(u) == tp;
+  std::cout << "segment(" << u << ") = " << table->get(u) << "\n";
+  return r;
+}
+
+uint ExtendedSegmentStack::getOutgoingEdge(uint u) {
+  if (graph->getNodeDegree(u) > graph->getOrder() / q) {
+    // to do: add big vertex storage in trailers
+    return 0;
+  } else {
+    unsigned f = edges->get(u);
+    unsigned g = graph->getNodeDegree(u) / l;
+    unsigned k1 = f * g;
+    return k1;
+  }
+}
+
+int ExtendedSegmentStack::getRestoreTrailer(Pair *r) {
+  if (tp > 1) {
+    *r = trailers[tp - 2];
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+void ExtendedSegmentStack::recolorLow(uint v) {
+  for (unsigned a = 0; a < q; a++) color->insert(low[a].head(), v);
+}
+
+bool ExtendedSegmentStack::isAligned() { return lp == q; }
