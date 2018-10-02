edbc42ba51ed93465d0c77f639507d13a6d47fe0
Simon Heuser <simon.heuser@mni.thm.de>
2018-10-02 08:00:33 +0200

    finish BFS visualisation (+ fixes for BFS!)

--- src-view/examples.cpp
+++ src-view/examples.cpp
@@ -4,6 +4,7 @@
 #include <sealibvisual/examples.h>
 #include <sealibvisual/tikzgenerator.h>
 #include <sealibvisual/tikzgraph.h>
+#include <iostream>
 using SealibVisual::Examples;
 using SealibVisual::TikzDocument;
 using SealibVisual::TikzPicture;
@@ -15,73 +16,93 @@ using Sealib::BFS;
 using Sealib::CompactArray;
 
 static TikzDocument *doc = nullptr;
+static std::shared_ptr<TikzGraph> tg;
+static std::shared_ptr<TikzPicture> pic;
 
-static std::shared_ptr<TikzGraph> bfs_vg;
-static std::shared_ptr<TikzPicture> bfs_pic;
-static uint bfs_inner = BFS_GRAY1, bfs_outer = BFS_GRAY2;
-static CompactArray *bfs_c;
+static unsigned bfs_inner = BFS_GRAY1, bfs_outer = BFS_GRAY2;
+static CompactArray *ca;
+
+const char *style_white = "circle,draw=black,fill=white",
+           *style_lightgray = "circle,draw=black,fill=gray!20",
+           *style_gray = "circle,draw=black,fill=gray!75",
+           *style_black = "circle,draw=black,fill=black";
 
 static void bfs_emit() {
-  doc->add(bfs_pic);
-  doc->add(TikzGenerator::generateTikzElement(*bfs_c, "color"));
+  doc->add(pic);
+  doc->add(TikzGenerator::generateTikzElement(*ca, "color"));
   doc->add("\\newpage");
 }
 
-static void vbfs_preprocess(uint u) {
-  bfs_vg->getNodes().at(u).setOptions("circle,draw=black,fill=gray!30");
-  bfs_c->insert(u, bfs_outer);
+static void bfs_preprocess(uint u) {
+  // std::cout << "preprocess " << u << "\n";
+  tg->getNodes().at(u).setOptions(style_lightgray);
+  ca->insert(u, bfs_outer);
   bfs_emit();
 }
-// static void vbfs_preexplore(uint u,uint v) {
-//   bfs_vg->getNodes().at(v).setOptions("circle,draw=black,fill=gray!70");
-//   bfs_c->insert(v, BFS_GRAY1);
-//   doc->add(bfs_pic);
-//   doc->add(TikzGenerator::generateTikzElement(*bfs_c, "color"));
-//   doc->add("\\newpage");
-// }
+static void bfs_preexplore(uint u, uint v) {
+  tg->getNodes().at(u).setOptions("circle,draw=black,fill=gray!70");
+  ca->insert(v, bfs_inner);
+  // bfs_emit();
+}
+
+static void bfs_swap() {
+  // swap gray if necessary:
+  bool swap = true;
+  for (uint f = 0; f < ca->size(); f++) {
+    if (ca->get(f) == bfs_inner) {
+      swap = false;
+      break;
+    }
+  }
+  if (swap) {
+    for (uint f = 0; f < ca->size(); f++) {
+      if (ca->get(f) == bfs_outer) {
+        tg->getNodes().at(f).setOptions(style_gray);
+      }
+    }
+    unsigned tmp = bfs_inner;
+    bfs_inner = bfs_outer;
+    bfs_outer = tmp;
+  }
+}
 
 TikzDocument *Examples::visualBFS(std::string filename) {
   doc =
       new TikzDocument(filename, "matrix,graphdrawing", "layered,force", true);
-  bfs_pic = std::shared_ptr<TikzPicture>(new TikzPicture("spring layout"));
+  pic = std::shared_ptr<TikzPicture>(
+      new TikzPicture("spring layout, sibling distance=15mm, node "
+                      "distance=20mm, node sep=1cm"));
   uint n = 10;
-  BasicGraph *g = Sealib::GraphCreator::createRandomFixed(n, 1);
-  bfs_vg = TikzGenerator::generateTikzElement(*g);
-  for (auto &e : bfs_vg->getEdges()) {
+
+  BasicGraph *g = Sealib::GraphCreator::createRandomFixed(n, 2);
+  for (uint a = 0; a < n; a++) {
+    std::cout << "node " << a << ": ";
+    for (uint b = 0; b < g->getNodeDegree(a); b++)
+      std::cout << g->head(a, b) << " ";
+    std::cout << "\n";
+  }
+  tg = TikzGenerator::generateTikzElement(*g);
+  ca = new CompactArray(n, 3);
+  BFS b(g, bfs_preprocess /*BFS_NOP_PROCESS*/,
+        bfs_preexplore /*BFS_NOP_EXPLORE*/);
+
+  for (auto &e : tg->getEdges()) {
     e.second.setOptions("->, color=black");
   }
-  bfs_pic->add(bfs_vg);
-  bfs_c = new CompactArray(n, 3);
-  BFS b(g, vbfs_preprocess, BFS_NOP_EXPLORE);
+  pic->add(tg);
   b.init();
-  while (b.more() || b.nextComponent()) {
-    uint u = b.next().head();
-    if (bfs_c->get(u) == bfs_inner) {
-      bfs_vg->getNodes().at(u).setOptions("circle,draw=white,fill=black");
-      bfs_c->insert(u, BFS_BLACK);
+
+  do {
+    while (b.more()) {
+      Pair p = b.next();
+
+      tg->getNodes().at(p.head()).setOptions(style_black);
+      ca->insert(p.head(), BFS_BLACK);
+      bfs_swap();
       bfs_emit();
-    } else {
-      bool gi = false;
-      for (uint d = 0; d < n; d++) {
-        if (bfs_c->get(d) == bfs_inner) {
-          gi = true;
-          break;
-        }
-      }
-      if (!gi) {
-        for (uint d = 0; d < n; d++) {
-          if (bfs_c->get(d) == bfs_outer) {
-            bfs_c->insert(d, bfs_inner);
-            bfs_vg->getNodes().at(d).setOptions(
-                "circle,draw=black,fill=gray!70");
-          }
-        }
-        uint tmp = bfs_inner;
-        bfs_inner = bfs_outer;
-        bfs_outer = tmp;
-        bfs_emit();
-      }
+      std::cout << p.head() << "," << p.tail() << " ";
     }
-  }
+    std::cout << "\n";
+  } while (b.nextComponent());
   return doc;
 }
