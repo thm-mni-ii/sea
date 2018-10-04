128eb9d13b26641a9763386f7c0ffdb2517cdb31
Johannes Meintrup <jmeintrup@googlemail.com>
2018-10-04 13:36:19 +0200

    merged graph and graphcreator from upstream master

--- src/basicgraph.cpp
+++ src/basicgraph.cpp
@@ -1,23 +1,38 @@
-#include <sealib/basicgraph.h>
+#include "sealib/basicgraph.h"
 
-const Sealib::Node &Sealib::Basicgraph::getNode(unsigned int u) { return nodes[u]; }
+using Sealib::BasicGraph;
+using Sealib::Node;
 
-unsigned int Sealib::Basicgraph::getNodeDegree(unsigned int u) {
-    return nodes[u].getDegree();
-}
-
-unsigned int Sealib::Basicgraph::head(unsigned int u, unsigned int k) {
-    return nodes[u].getAdj()[k].vertex;
-}
-
-unsigned int Sealib::Basicgraph::getOrder() {
-    return static_cast<unsigned int>(nodes.size());
-}
-
-Sealib::Basicgraph::Basicgraph(const std::vector<Sealib::Node> &nodes_) : nodes(nodes_) {}
-
-Sealib::Basicgraph::Basicgraph(Sealib::Node *nodes_, unsigned int order_) : nodes(order_) {
-    for (unsigned int i = 0; i < order_; ++i) {
+Sealib::BasicGraph::BasicGraph(Node *nodes_, unsigned int order_) : nodes(order_){
+    for(unsigned int i = 0; i < order_; i++) {
         nodes[i] = nodes_[i];
     }
+}
+Sealib::BasicGraph::BasicGraph(const std::vector<Sealib::Node> &nodes_) : nodes(nodes_){
+
+}
+Sealib::BasicGraph::BasicGraph() : nodes(0){
+
+}
+Sealib::BasicGraph::BasicGraph(unsigned int order) : nodes(order){
+
+}
+
+void Sealib::BasicGraph::addNode(const Sealib::Node &node) {
+    nodes.emplace_back(node);
+}
+const Node &Sealib::BasicGraph::getNode(unsigned int u) const {
+    return nodes[u];
+}
+Node &Sealib::BasicGraph::getNode(unsigned int u) {
+    return nodes[u];
+}
+unsigned int Sealib::BasicGraph::getNodeDegree(unsigned int u) const {
+    return nodes[u].getDegree();
+}
+unsigned int Sealib::BasicGraph::head(unsigned int u, unsigned int k) const {
+    return nodes[u].getAdj()[k].vertex;
+}
+unsigned int Sealib::BasicGraph::getOrder() const {
+    return static_cast<unsigned int>(nodes.size());
 }
