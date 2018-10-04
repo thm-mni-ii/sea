bc977c1820bcf0d3573e10fc7320d3fe2c34568f
Johannes Meintrup <jmeintrup@googlemail.com>
2018-10-04 12:14:46 +0200

    Added complete constructor for RecursiveSubGraph. Added reference graph index.

--- src/basesubgraph.cpp
+++ src/basesubgraph.cpp
@@ -7,37 +7,29 @@
  */
 Sealib::BaseSubGraph::~BaseSubGraph() = default;
 
-Sealib::BaseSubGraph::BaseSubGraph(rgraph_t rGraph_) :
-    SubGraph(initializeQSelect(rGraph_), initializePSelect(rGraph_)),
-    rGraph(rGraph_) {}
-
-Sealib::BaseSubGraph::bitset_t
-Sealib::BaseSubGraph::initializeQSelect(Sealib::BaseSubGraph::rgraph_t rGraph) {
-    bitset_t bs(rGraph->getOrder());
+Sealib::BaseSubGraph::BaseSubGraph(stack_t *stack, rgraph_t rGraph_) :
+    SubGraph(0, 0, stack),
+    rGraph(rGraph_) {
+    unsigned long dSum = 0;
+    bitset_t q(rGraph->getOrder());
     for (unsigned int i = 0; i < rGraph->getOrder(); i++) {
-        if (rGraph->getNodeDegree(i) > 0) {
-            bs[i] = 1;
+        unsigned long deg = rGraph->getNodeDegree(i);
+        dSum += deg;
+        if (deg > 0) {
+            q[i] = 1;
         }
     }
-    return bs;
-}
 
-Sealib::BaseSubGraph::bitset_t
-Sealib::BaseSubGraph::initializePSelect(Sealib::BaseSubGraph::rgraph_t rGraph) {
-    unsigned long dSum = 0;
-    for (unsigned int i = 0; i < rGraph->getOrder(); i++) {
-        dSum += rGraph->getNodeDegree(i);
-    }
-
-    bitset_t bs(dSum + 1);
+    bitset_t p(dSum);
     unsigned long deg = 0;
     for (unsigned int i = 0; i < rGraph->getOrder(); i++) {
         if (rGraph->getNodeDegree(i) > 0) {
             deg += rGraph->getNodeDegree(i);
-            bs[deg] = 1;
+            p[deg - 1] = 1;
         }
     }
-    return bs;
+    qSelect = new RankSelect(q);
+    pSelect = new RankSelect(p);
 }
 
 unsigned long Sealib::BaseSubGraph::head(unsigned long u,
@@ -48,25 +40,6 @@ unsigned long Sealib::BaseSubGraph::head(unsigned long u,
 std::tuple<unsigned long, unsigned long>
 Sealib::BaseSubGraph::mate(unsigned long u, unsigned long k) const {
     return rGraph->mate(static_cast<unsigned int>(u), static_cast<unsigned int>(k));
-}
-
-unsigned long Sealib::BaseSubGraph::degree(unsigned long u) const {
-    if (u == 0) {
-        throw std::invalid_argument("u needs to be > 0");
-    }
-    unsigned long a = select_p(rank_q(u));  //  pSelect.select(qSelect.rank(v));
-    if (a == (unsigned long) -1) {
-        throw std::out_of_range("out of range on node idx v");
-    }
-    unsigned long b = select_p(rank_q(u - 1));  // pSelect.select(qSelect.rank(v - 1));
-
-    if (a == b) {
-        return 0;
-    } else if (b == (unsigned long) -1) {
-        return a - 1;
-    } else {
-        return a - b;
-    }
 }
 
 unsigned long Sealib::BaseSubGraph::phi(unsigned long u) const {
