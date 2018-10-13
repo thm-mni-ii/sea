cb1e0b708b426733bb25fc2d75e60e287e48576e
Simon Heuser <simon.heuser@mni.thm.de>
2018-10-13 11:47:05 +0200

    add legacy tests and DFS

--- include/sealib/legacy.h
+++ include/sealib/legacy.h
@@ -6,6 +6,7 @@ extern "C" {
 
 void *Sealib_Graph_new(unsigned int **m, unsigned int order);
 void Sealib_Graph_delete(void *self);
+void *Sealib_Graph_generateRandom(unsigned int order);
 
 void *Sealib_ChoiceDictionary_new(unsigned int size);
 void Sealib_ChoiceDictionary_delete(void *self);
@@ -20,6 +21,7 @@ int Sealib_ChoiceDictionaryIterator_more(void *self);
 unsigned long Sealib_ChoiceDictionaryIterator_next(void *self);
 
 void *Sealib_Bitset_new(unsigned long size);
+void Sealib_Bitset_delete(void *self);
 void Sealib_Bitset_set(void *self, unsigned long index);
 int Sealib_Bitset_get(void *self, unsigned long index);
 
@@ -28,6 +30,11 @@ void Sealib_RankSelect_delete(void *self);
 unsigned long Sealib_RankSelect_rank(void *self, unsigned long index);
 unsigned long Sealib_RankSelect_select(void *self, unsigned long bit);
 unsigned long Sealib_RankSelect_size(void *self);
+
+void Sealib_DFS_nloglognBitDFS(void *graph, void (*preprocess)(unsigned int),
+                               void (*preexplore)(unsigned int, unsigned int),
+                               void (*postexplore)(unsigned int, unsigned int),
+                               void (*postprocess)(unsigned int));
 
 #ifdef __cplusplus
 }
