491991fcfea5bfc9ef35536396def894e642b219
Johannes Meintrup <jmeintrup@googlemail.com>
2018-10-26 14:07:06 +0200

    Added explanatory comments to trackingallocator.h

--- include/sealib/trackingallocator.h
+++ include/sealib/trackingallocator.h
@@ -16,6 +16,9 @@ namespace Sealib {
 template<class T>
 class TrackingAllocator {
  public:
+    /**
+     * These definitions are needed for the allocator interface.
+     */
     using value_type = T;
     using pointer = T *;
     using const_pointer = const T *;
@@ -24,6 +27,11 @@ class TrackingAllocator {
     using size_type = size_t;
     using difference_type = std::ptrdiff_t;
 
+    /**
+     * Needed to rebind a TrackingAllocator<U> to TrackingAllocator<U'>,
+     * if needed by the container using TrackingAllocator.
+     * @tparam U
+     */
     template<class U>
     struct rebind {
       using other = TrackingAllocator<U>;
@@ -32,37 +40,80 @@ class TrackingAllocator {
     TrackingAllocator() = default;
     ~TrackingAllocator() = default;
 
+    /**
+     * Needed to rebind a TrackingAllocator<U> to TrackingAllocator<U'>,
+     * if needed by the container using TrackingAllocator.
+     * @tparam U
+     */
     template<class U>
     TrackingAllocator(const TrackingAllocator<U> &other) {}
 
+    /**
+     * Allocates numObjects *sizeof(T) * bytes space and writes that
+     * number into a counter that tracks all allocated memory.
+     * Otherwise works exactly as std::allocator<T>
+     */
     pointer allocate(size_type numObjects) {
         ByteCounter::get() += sizeof(T) * numObjects;
         return static_cast<pointer>(operator new(sizeof(T) * numObjects));
     }
 
+    /**
+     * Allocates numObjects *sizeof(T) * bytes space and writes that
+     * number into a counter that tracks all allocated memory.
+     * Otherwise works exactly as std::allocator<T>
+     * The param hint is generally used as a pointer to some
+     * free memory that might be able to hold the requested memory.
+     * Used to speed up allocation if a custom allocator implements it.
+     * We do not implement it here, but instead just want the regular allocation
+     * function to be called. This is rarely needed.
+     */
     pointer allocate(size_type numObjects, const_void_pointer hint) {
         return allocate(numObjects);
     }
 
+    /**
+     * Deallocates memory that was previously allocated at pointer p of the size
+     * of numObjects * sizeof(T). Substracts the now freed memory from the global
+     * counter ByteCounter.
+     * Otherwise works exactly as std::allocator::deallocate
+     */
     void deallocate(pointer p, size_type numObjects) {
         ByteCounter::get() -= sizeof(T) * numObjects;
         operator delete(p);
     }
 
+    /**
+     * Maximum size possibly for allocation, depends on hardware used.
+     */
     size_type max_size() const {
         return std::numeric_limits<size_type>::max();
     }
 
+    /**
+     * Construct is used for the construction of the objects after allocation.
+     * Same as std::allocator
+     */
     template<class U, class... Args>
     void construct(U *p, Args &&...args) {
         new(p) U(std::forward<Args>(args)...);
     }
 
+    /**
+     * Destroy is used on the objects before deallocation.
+     * Same as std::allocator
+     */
     template<class U>
     void destroy(U *p) {
         p->~U();
     }
 
+    /**
+     * @return - currently allocated bytes by all TrackingAllocator classes.
+     *
+     * Important: Even though TrackingAllocator is a templated class,
+     * all versions of the TrackingAllocator share the same ByteCounter!
+     */
     template<class U>
     size_type count() {
         return ByteCounter::get();
