dd3ff886ef4bdeab6ded458c52d3f9da04966ad4
Johannes Meintrup <jmeintrup@googlemail.com>
2018-11-05 22:16:14 +0100

    Templated the localdycktable

--- include/sealib/localselecttable.h
+++ include/sealib/localselecttable.h
@@ -3,19 +3,29 @@
 #include <vector>
 
 namespace Sealib {
+
+template <typename BlockType = unsigned char>
+class LocalSelectTable;
+}
+
+namespace Sealib {
 /**
 * Used for RankSelect to lookup select values in local segments of size 8-bits.
 * All values are stored in a lookup table.
 * @author Johannes Meintrup
 */
+template <typename BlockType>
 class LocalSelectTable {
  private:
-    std::vector<std::vector<unsigned char>> localSelectLookupTable;
+    std::vector<std::vector<BlockType>> localSelectLookupTable;
     /**
      * Used in getLocalSelect for the first initialization of the table, never used again.
      */
     LocalSelectTable();
     ~LocalSelectTable();
+
+    static const constexpr unsigned int bitsPerBlock = sizeof(BlockType) * 8;
+    static const constexpr BlockType blockTypeMax = (BlockType) - 1;
 
  public:
     /**
@@ -23,9 +33,9 @@ class LocalSelectTable {
     * The lookup table is a static instance in this function that is initialized on the first call.
     * @param segment - unsinged char representing a bit vector of size 8
     * @param i-th set bit to be selected
-    * @return index of the i-th bit, or (unsinged char) - 1 if there is none
+    * @return index of the i-th bit, or (BlockType) - 1 if there is none
     */
-    static unsigned char getLocalSelect(unsigned char segment, unsigned char i);
+    static BlockType getLocalSelect(BlockType segment, BlockType i);
 
     //  singleton instance only in the function getLocalSelect, these should not be used
     LocalSelectTable(LocalSelectTable const &) = delete;
