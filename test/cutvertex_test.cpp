58015db897b723425c47bc158913aacb6bd1a491
Simon Heuser <simon.heuser@mni.thm.de>
2018-11-06 10:45:08 +0100

    add cut vertex iterator with first DFS run

---
+++ test/cutvertex_test.cpp
@@ -0,0 +1,11 @@
+#include "sealib/cutvertex.h"
+#include <gtest/gtest.h>
+#include "sealib/graphcreator.h"
+
+using namespace Sealib;  // NOLINT
+
+TEST(CutVertexIteratorTest, init) {
+    std::pair<BasicGraph*, uint> p =
+        GraphCreator::createRandomUndirected(100, 3);
+    CutVertexIterator c(p.first);
+}
