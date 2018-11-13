c3b5a7b1dc72804fae3ab31ae6982ce2b7e21d33
Simon Heuser <simon.heuser@mni.thm.de>
2018-11-13 10:14:54 +0100

    add graph constructor for SSA, add BCC iterator init()

--- include/sealib/staticspacestorage.h
+++ include/sealib/staticspacestorage.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include "sealib/_types.h"
 #include "sealib/bitset.h"
+#include "sealib/graph.h"
 #include "sealib/rankselect.h"
 #include "sealib/sequence.h"
 
@@ -37,6 +38,13 @@ class StaticSpaceStorage : public Sequence<uint64_t> {
      *      => 4 data packs: size 3, 2, 0 and 2 bits
      */
     explicit StaticSpaceStorage(const std::vector<bool> &bits);
+
+    /**
+     * Create a new storage with O(log(deg(u)) bits for each node u in the graph
+     * G.
+     * @param g graph G=(V,E) to create a storage for
+     */
+    explicit StaticSpaceStorage(Graph *g);
 
     /**
      * Convenience method to create a bit pattern from a vector of sizes
--- src/bcciterator.cpp
+++ src/bcciterator.cpp
@@ -2,15 +2,30 @@
 
 using Sealib::BCCIterator;
 
-void BCCIterator::init() {}
+BCCIterator::BCCIterator(BasicGraph *graph)
+    : g(graph), n(g->getOrder()), e(g) {}
 
-void BCCIterator::setOrigin(uint u, uint v) {}
+BCCIterator::BCCIterator(BasicGraph *graph, EdgeMarker *edges)
+    : g(graph), n(g->getOrder()), e(*edges) {}
+
+void BCCIterator::init() {
+    CompactArray color(n, 3);
+    for (uint a = 0; a < n; a++) color.insert(a, DFS_WHITE);
+    StaticSpaceStorage parent(g);
+
+    e.identifyEdges(&color, &parent);
+
+    for (uint a = 0; a < n; a++) {
+        color.insert(a, DFS_WHITE);
+    }
+    e.markTreeEdges(&color, &parent);
+}
+
+void BCCIterator::init(uint u, uint v) {
+    startEdge = Pair(u, v);
+    init();
+}
 
 bool BCCIterator::more() { return false; }
 
 uint BCCIterator::next() { return 0; }
-
-BCCIterator::BCCIterator(BasicGraph *graph) : g(graph), e(g) {}
-
-BCCIterator::BCCIterator(BasicGraph *graph, EdgeMarker *edges)
-    : g(graph), e(*edges) {}
