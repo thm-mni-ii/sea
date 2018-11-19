bafee60ed7b93605aead582da8fc4d19c56dc4ec
Johannes Meintrup <jmeintrup@googlemail.com>
2018-11-19 14:19:48 +0100

    Moved template implementation to header file for localselecttable (templates should not be implemented in .cpp)

--- include/sealib/localselecttable.h
+++ include/sealib/localselecttable.h
@@ -1,5 +1,8 @@
 #ifndef SEALIB_LOCALSELECTTABLE_H_
 #define SEALIB_LOCALSELECTTABLE_H_
+
+#define CHECK_BIT(var, pos) (((var)>>(pos)) & 1)
+
 #include <vector>
 
 namespace Sealib {
@@ -22,14 +25,13 @@ class LocalSelectTable {
      * Used in getLocalSelect for the first initialization of the table, never used again.
      */
     LocalSelectTable();
-    ~LocalSelectTable();
 
     static const unsigned int bitsPerBlock = sizeof(BlockType) * 8;
-    static const BlockType blockTypeMax = (BlockType) - 1;
+    static const BlockType blockTypeMax = std::numeric_limits<BlockType>::max();
 
 
  public:
-    static const BlockType npos = blockTypeMax;
+    static const BlockType npos = std::numeric_limits<BlockType>::max();
     /**
     * Values are stored in a lookup table.
     * The lookup table is a static instance in this function that is initialized on the first call.
@@ -37,11 +39,31 @@ class LocalSelectTable {
     * @param i-th set bit to be selected
     * @return index of the i-th bit, or (BlockType) - 1 if there is none
     */
-    static BlockType getLocalSelect(BlockType segment, BlockType i);
+    static BlockType getLocalSelect(size_t b, size_t i);
 
     //  singleton instance only in the function getLocalSelect, these should not be used
     LocalSelectTable(LocalSelectTable const &) = delete;
     void operator=(LocalSelectTable const &) = delete;
 };
+template <typename BlockType>
+LocalSelectTable<BlockType>::LocalSelectTable() : localSelectLookupTable(static_cast<size_t>(blockTypeMax+1)) {
+    for (unsigned int i = 0; i <= blockTypeMax; i++) {
+        BlockType rank = 0;
+        localSelectLookupTable[i] = std::vector<BlockType>(
+            bitsPerBlock,
+            std::numeric_limits<BlockType>::max());
+        for (BlockType j = 0; j < bitsPerBlock; j++) {
+            if (CHECK_BIT(i, j)) {
+                localSelectLookupTable[i][rank++] = j;
+            }
+        }
+    }
+}
+template <typename BlockType>
+BlockType LocalSelectTable<BlockType>::getLocalSelect(size_t b,
+                                                      size_t i) {
+    static LocalSelectTable<BlockType> instance;
+    return instance.localSelectLookupTable[b][i];
+}
 }  // namespace Sealib
 #endif  // SEALIB_LOCALSELECTTABLE_H_
