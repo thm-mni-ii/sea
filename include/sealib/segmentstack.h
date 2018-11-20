3b2e982fdb4d3b3a74df043d0000a8cf74b36835
Simon Heuser <simon.heuser@mni.thm.de>
2018-11-20 14:59:20 +0100

    add comments to segment stack

--- include/sealib/segmentstack.h
+++ include/sealib/segmentstack.h
@@ -26,18 +26,18 @@ namespace Sealib {
 */
 class SegmentStack {
  public:
-  virtual int push(std::pair<uint, uint> u) = 0;
-  int pop(std::pair<uint, uint> *r);
-  bool isEmpty();
-  virtual bool isAligned() = 0;
+    virtual void push(std::pair<uint, uint> u) = 0;
+    int pop(std::pair<uint, uint> *r);
+    bool isEmpty();
+    virtual bool isAligned() = 0;
 
  protected:
-  explicit SegmentStack(unsigned segmentSize);
-  virtual ~SegmentStack();
+    explicit SegmentStack(unsigned segmentSize);
+    virtual ~SegmentStack();
 
-  unsigned q;
-  std::pair<uint, uint> *low, *high;
-  unsigned lp, hp, tp;
+    unsigned q;
+    std::pair<uint, uint> *low, *high;
+    unsigned lp, hp, tp;
 };
 
 /**
@@ -51,22 +51,33 @@ class SegmentStack {
  */
 class BasicSegmentStack : public SegmentStack {
  public:
-  explicit BasicSegmentStack(unsigned segmentSize);
+    explicit BasicSegmentStack(unsigned segmentSize);
 
-  int push(std::pair<uint, uint> u) override;
-  // is the restoration finished? (i.e. saved trailer and actual trailer are
-  // aligned)
-  bool isAligned() override;
+    /**
+     * Push a tuple on the stack.
+     * @param u Tuple to store on the stack
+     *
+     */
+    void push(std::pair<uint, uint> u) override;
+    /**
+     * @return true if the restoration is finished (i.e. saved trailer and
+     * actual trailer are aligned)
+     */
+    bool isAligned() override;
 
-  // empty the entire stack - needed for a full restoration */
-  void dropAll();
-  // save a trailer to survive a full restoration */
-  void saveTrailer();
+    /**
+     * Empty the entire stack. (Needed for a full restoration)
+     */
+    void dropAll();
+    /**
+     * Save a trailer to survive a full restoration.
+     */
+    void saveTrailer();
 
  private:
-  std::pair<uint, uint> last;
-  std::pair<uint, uint> savedTrailer;
-  int alignTarget;
+    std::pair<uint, uint> last;
+    std::pair<uint, uint> savedTrailer;
+    int alignTarget;
 };
 
 /**
@@ -75,58 +86,96 @@ class BasicSegmentStack : public SegmentStack {
  */
 class ExtendedSegmentStack : public SegmentStack {
  public:
-  // the extended stack needs access to the graph and the color array; the
-  // segment size is n/ld(n) by default
-  ExtendedSegmentStack(uint size, Graph *g, CompactArray *c);
-  ~ExtendedSegmentStack();
+    /**
+     * Create a new ExtendedSegmentStack. It needs access to the DFS's graph and
+     * the color array. The segment size is n/ld(n) by default.
+     * @param size Number of nodes in the graph
+     * @param g Directed graph G=(V,E)
+     * @param c Compact array holding color values
+     */
+    ExtendedSegmentStack(uint size, Graph *g, CompactArray *c);
+    ~ExtendedSegmentStack();
 
-  int push(std::pair<uint, uint> u) override;
-  bool isAligned() override;
+    void push(std::pair<uint, uint> u) override;
 
-  // check if u is labeled with the top segment number (i.e. is table[u]=top?)
-  // @param restoring refer to the previous top segment (used when restoring and
-  // values are actively pushed)
-  bool isInTopSegment(uint u, bool restoring = false);
-  // get the outgoing edge: retrieve the approximation from the table if u is
-  // small, get the edge from the trailer stack if u is big
-  uint getOutgoingEdge(uint u);
-  // get the second-last trailer (the one that is needed to do a 1-segment
-  // restoration)
-  // @return 0 if a trailer is found, 1 otherwise
-  int getRestoreTrailer(std::pair<uint, uint> *r);
-  // get the last trailer (the one that a restoration will align to)
-  int getTopTrailer(std::pair<uint, uint> *r);
-  // recolor all vertices in the low segment to the given color (used after a
-  // restoration when the low segment is actually the top segment of S)
-  void recolorLow(unsigned v);
+    /**
+     * @return true if saved trailer and actual trailer are aligned
+     */
+    bool isAligned() override;
 
-  unsigned approximateEdge(uint u, uint k);
-  uint retrieveEdge(uint u, unsigned f);
+    /**
+     * Check if u is labeled with the top segment number (i.e. is table[u]=top?)
+     * @param u Node to check
+     * @param restoring Refer to the previous top segment? (used when restoring
+     * and
+     * values are actively pushed)
+     */
+    bool isInTopSegment(uint u, bool restoring = false);
+    /**
+     * Get the outgoing edge for a given node. Retrieve the approximation from
+     * the table if u is small, get the edge from the trailer stack if u is big.
+     * @param u Node to get the edge for
+     */
+    uint getOutgoingEdge(uint u);
+    /**
+     * Get the second-last trailer (the one that is needed to do a 1-segment
+     * restoration).
+     * @param r Pointer to the tuple that will hold the trailer
+     * @return 0 if a trailer is found, 1 otherwise
+     */
+    int getRestoreTrailer(std::pair<uint, uint> *r);
+    /**
+     * Get the last trailer (the one that a restoration will align to).
+     * @param r Pointer to the tuple that will hold the trailer
+     * @return 0 if a trailer is found, 1 otherwise
+     */
+    int getTopTrailer(std::pair<uint, uint> *r);
+    /**
+     * Recolor all vertices in the low segment to the given color (used after a
+     * restoration when the low segment is actually the top segment of S).
+     * @param v Color to apply to the low segment
+     */
+    void recolorLow(unsigned v);
+
+    /**
+     * Get the edge approximation for a tuple (u,k)
+     * @param u Node number
+     * @param k Outgoing edge index
+     * @return Approximation: f = floor((k-1)/ceil(deg(u)/ld(n)))
+     */
+    unsigned approximateEdge(uint u, uint k);
+    /**
+     * Get the outgoing edge index from a given approximation.
+     * @param u Node number
+     * @param f Approximation
+     * @return Outgoing edge: k = ceil(deg(u)/ld(n))*floor((k-1)/gu)
+     */
+    uint retrieveEdge(uint u, unsigned f);
 
  private:
-  /**
-   * A Trailer struct additionally manages a sequence of big vertices.
-   * In detail: trailers[tp-1] manages all big vertices in its segment. The
-   * first managed vertex can be accessed via big[trailers[tp-1].bi].
-   * bc is freely available to increment during restoration.
-   */
-  struct Trailer {
-    std::pair<uint, uint> x;
-    unsigned bi, bc;
-    Trailer() : bi(INVALID), bc(0) {}
-  };
+    /**
+     * A Trailer struct additionally manages a sequence of big vertices.
+     * In detail: trailers[tp-1] manages all big vertices in its segment. The
+     * first managed vertex can be accessed via big[trailers[tp-1].bi].
+     * bc is freely available to increment during restoration.
+     */
+    struct Trailer {
+        std::pair<uint, uint> x;
+        unsigned bi, bc;
+        Trailer() : bi(INVALID), bc(0) {}
+    };
 
-  Trailer *trailers;
-  unsigned l;
-  CompactArray *table, *edges;
-  std::pair<uint, uint> *big;
-  unsigned bp;
-  Graph *graph;
-  unsigned m, n;
-  CompactArray *color;
+    Trailer *trailers;
+    unsigned l;
+    CompactArray *table, *edges;
+    std::pair<uint, uint> *big;
+    unsigned bp;
+    Graph *graph;
+    unsigned m, n;
+    CompactArray *color;
 
-  static constexpr unsigned INVALID = static_cast<unsigned>(-1);
-  void storeEdges();
+    static constexpr unsigned INVALID = static_cast<unsigned>(-1);
+    void storeEdges();
 };
 }  // namespace Sealib
 #endif  // SEALIB_SEGMENTSTACK_H_
