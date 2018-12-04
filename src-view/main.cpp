153627eb8530613c6a04f481b17a23c3ee9fb123
Simon Heuser <simon.heuser@mni.thm.de>
2018-12-04 15:33:59 +0100

    clean up src-view/main.cpp

--- src-view/main.cpp
+++ src-view/main.cpp
@@ -1,19 +1,5 @@
 #include <sealib/_types.h>
-#include <sealib/graph/compactgraph.h>
 #include <sealib/graph/graphcreator.h>
-#include <sealib/graph/graphrepresentations.h>
-#include <sealib/iterator/choicedictionaryiterator.h>
-#include <sealib/iterator/dfs.h>
-#include <sealib/runtimetest.h>
-#include <chrono>
-#include <cmath>
-#include <ctime>
-#include <iostream>
-#include <memory>
-#include <random>
-#include <sstream>
-#include <stack>
-#include <string>
 #include "./test_visual.h"
 #include "sealibvisual/examples.h"
 #include "sealibvisual/tikzarray.h"
@@ -25,14 +11,7 @@
 #include "sealibvisual/tikzpicture.h"
 #include "sealibvisual/tikzstyle.h"
 
-using std::cout;
-using std::stack;
-using namespace Sealib;        // NOLINT
-using namespace SealibVisual;  // NOLINT
-
-void tikz_example() {}
-
-void tikz_example2() {
+void tikz_example() {
     unsigned int order = 4;
     auto **adj_mtrx = (unsigned int **)malloc(sizeof(unsigned int) * 4 * 4);
     adj_mtrx[0] = new unsigned int[order]{0, 1, 0, 1};
@@ -42,7 +21,7 @@ void tikz_example2() {
 
     Sealib::UndirectedGraph bg =
         Sealib::GraphCreator::createGraphFromAdjacencyMatrix(adj_mtrx, order);
-    std::shared_ptr<SealibVisual::TikzElement> vg =
+    std::shared_ptr<SealibVisual::TikzGraph> vg =
         SealibVisual::TikzGenerator::generateTikzElement(&bg);
 
     std::shared_ptr<SealibVisual::TikzPicture> pic(
@@ -58,7 +37,7 @@ void tikz_example2() {
         "out.tex", "matrix, backgrounds, graphdrawing", "force", true);
     doc.add(fig);
 
-    for (auto &e : ((SealibVisual::TikzGraph *)(vg.get()))->getEdges()) {
+    for (auto &e : vg->getEdges()) {
         e.second.setOptions("--, color=green, line width=8");
     }
     doc.add(fig);
@@ -80,74 +59,4 @@ void tikz_example2() {
     doc.close();
 }
 
-/*void tikz_example3() {
-    DirectedGraph g = Sealib::GraphCreator::createRandomKRegularGraph(10, 4);
-    std::shared_ptr<TikzGraph> vg = TikzGenerator::generateTikzElement(&g);
-    TikzGraph *tg = vg.get();
-    for (auto &e : tg->getEdges()) {
-        e.second.setOptions("line width=1pt, color=black, arrows={->}");
-    }
-
-    Sealib::CompactArray c(10);
-    std::shared_ptr<TikzPicture> vc =
-        TikzGenerator::generateTikzElement(c, 10, "color");
-
-    std::shared_ptr<TikzPicture> pic1(new TikzPicture("spring layout"));
-    std::shared_ptr<TikzPicture> pic2(new TikzPicture(""));
-
-    pic1->add(vg);
-
-    TikzDocument doc("out-bfs.tex", "matrix,positioning,graphdrawing",
-                     "layered,force", true);
-    doc.add(pic1);
-    // doc.add(pic2);
-    doc.add(vc);
-    doc.add("\\newpage");
-
-    tg->getNodes().at(0).setOptions("circle,draw=black,fill=gray");
-    c.insert(0, 1);
-    doc.add(pic1);
-    doc.add(TikzGenerator::generateTikzElement(c, 10, "color"));
-    doc.add("\\newpage");
-
-    tg->getNodes().at(0).setOptions("circle,draw=black,fill=gray");
-    c.insert(0, 1);
-    doc.add(pic1);
-    doc.add(TikzGenerator::generateTikzElement(c, 10, "color"));
-    doc.add("\\newpage");
-
-    tg->getNodes().at(1).setOptions("circle,draw=black,fill=gray!30");
-    tg->getNodes().at(5).setOptions("circle,draw=black,fill=gray!30");
-    tg->getNodes().at(7).setOptions("circle,draw=black,fill=gray!30");
-    tg->getNodes().at(8).setOptions("circle,draw=black,fill=gray!30");
-    c.insert(1, 2);
-    c.insert(5, 2);
-    c.insert(7, 2);
-    c.insert(8, 2);
-    doc.add(pic1);
-    doc.add(TikzGenerator::generateTikzElement(c, 10, "color"));
-    doc.add("\\newpage");
-
-    tg->getNodes().at(0).setOptions("circle,draw=black,fill=black");
-    tg->getNodes().at(1).setOptions("circle,draw=black,fill=gray");
-    tg->getNodes().at(5).setOptions("circle,draw=black,fill=gray");
-    tg->getNodes().at(7).setOptions("circle,draw=black,fill=gray");
-    tg->getNodes().at(8).setOptions("circle,draw=black,fill=gray");
-    c.insert(0, 3);
-    c.insert(1, 1);
-    c.insert(5, 1);
-    c.insert(7, 1);
-    c.insert(8, 1);
-    doc.add(pic1);
-    doc.add(TikzGenerator::generateTikzElement(c, 10, "color"));
-    doc.add("\\newpage");
-
-    doc.close();
-
-    // Convert PDF to GIF:
-    // convert -density 200 -delay 100 -background white -alpha remove
-out-bfs.pdf out-bfs.gif
-
-}*/
-
-int main() { VisualTest::testBFS(); }
+int main() { SealibVisual::VisualTest::testDFS(); }
