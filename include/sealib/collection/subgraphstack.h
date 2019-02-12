d0ae1b8a8b0420a107cfc02ab70f5eaefb426c7f
Johannes Meintrup <jmeintrup@googlemail.com>
2019-02-12 11:09:04 +0100

    added a few rvalue constructors, rankselect, subgraphstack, trailstructure and dyckmatching

--- include/sealib/collection/subgraphstack.h
+++ include/sealib/collection/subgraphstack.h
@@ -56,6 +56,8 @@ class SubGraphStack {
      */
     void push(const Sealib::Bitset<uint8_t> &v,
               const Sealib::Bitset<uint8_t> &a);
+    void push(Sealib::Bitset<uint8_t> &&v,
+              Sealib::Bitset<uint8_t> &&a);
 
     /**
      * Pushes a new subgraph G_l+1 on G_l.
@@ -67,6 +69,7 @@ class SubGraphStack {
      * @param a Bitsequence of length 2m_l
      */
     void push(const Sealib::Bitset<uint8_t> &a);
+    void push(Bitset<uint8_t> &&a);
 
     /**
      * Replaces the client list (G_0,...,G_l) with (G_0,...,G_l-1).
