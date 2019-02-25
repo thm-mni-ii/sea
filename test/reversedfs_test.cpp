5d7aaca8badc8b3cb2f9f0558654ea5debbedd24
Simon Heuser <simon.heuser@mni.thm.de>
2019-02-25 16:55:25 +0100

    expanded tests for reverse DFS

--- test/reversedfs_test.cpp
+++ test/reversedfs_test.cpp
@@ -7,7 +7,7 @@
 
 using namespace Sealib;  // NOLINT
 
-static uint64_t GRAPHCOUNT = 4, ORDER = 500, DEGREE = 25;
+static uint64_t GRAPHCOUNT = 8, ORDER = 2500, DEGREE = 16;
 
 static std::vector<DirectedGraph> makeGraphs() {
     std::vector<DirectedGraph> g;
@@ -23,26 +23,27 @@ INSTANTIATE_TEST_CASE_P(ParamTests, ReverseDFSTest,
                         ::testing::ValuesIn(makeGraphs()),
                         /**/);
 
-TEST_P(ReverseDFSTest, postprocess) {
+TEST_P(ReverseDFSTest, comparison) {
     DirectedGraph g = GetParam();
     ReverseDFS r(g);
-    SimpleReverseDFS s(g, UserCall::postprocess);
+    SimpleReverseDFS s(g);
     r.init();
     s.init();
     std::vector<UserCall> v1, v2;
     while (r.more()) {
         UserCall a = r.next();
-        if (a.type == UserCall::postprocess) v1.push_back(a);
+        v1.push_back(a);
     }
     while (s.more()) {
         UserCall a = s.next();
-        if (a.type == UserCall::postprocess) v2.push_back(a);
+        v2.push_back(a);
     }
-    ASSERT_EQ(v1.size(), v2.size());
+    EXPECT_EQ(v1.size(), v2.size());
     bool equal = true;
+    uint64_t n = v1.size() <= v2.size() ? v1.size() : v2.size();
     // printf("[ ");
-    for (uint64_t a = 0; a < v1.size(); a++) {
-        if (v1[a].u != v2[a].u) {
+    for (uint64_t a = 0; a < n; a++) {
+        if (v1[a].u != v1[a].u) {
             equal = false;
             // printf(" <<%lu!=%lu>> ", v1[a].u, v2[a].u);
         } else {
