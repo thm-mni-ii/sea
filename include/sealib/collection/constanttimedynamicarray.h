c3c3c9fc4aa2d604fe5acec0620b8c3a49830566
Johannes <johannes.meintrup@mni.thm.de>
2019-06-02 21:32:13 +0200

    Fixed segmentation fault.

--- include/sealib/collection/constanttimedynamicarray.h
+++ include/sealib/collection/constanttimedynamicarray.h
@@ -21,7 +21,8 @@ class ConstantTimeDynamicArray {
         : _size(size),
           init(init),
           border(0),
-          A(new uint64_t[(size % 2 ? size : size + 1)]) {}
+          _sizeA((size % 2 == 0 ? size : size + 1)),
+          A(new uint64_t[(size % 2 == 0 ? size : size + 1)]) {}
 
     /**
      * Fills the array with the specified value in constant time.
@@ -41,9 +42,6 @@ class ConstantTimeDynamicArray {
      */
     proxy operator[](uint64_t i) { return proxy(this, i); }
     uint64_t operator[](uint64_t i) const { return get(i); }
-
-    void insert(uint64_t i, uint64_t value);
-    uint64_t get(uint64_t i) const;
 
     class proxy {
      public:
@@ -97,6 +95,7 @@ class ConstantTimeDynamicArray {
     uint64_t _size;
     uint64_t init;
     uint64_t border;
+    uint64_t _sizeA;
     std::unique_ptr<uint64_t[]> A;
 
     bool isChain(uint64_t i) const;
@@ -105,14 +104,15 @@ class ConstantTimeDynamicArray {
     void breakChain(uint64_t i);
     void initBlock(uint64_t i);
     uint64_t extend();
-
-    friend class proxy;
+    void insert(uint64_t i, uint64_t value);
+    uint64_t get(uint64_t i) const;
 };
 struct NotChained : public std::exception {
     const char* what() const noexcept override { return "NotChained"; }
 };
 
 void ConstantTimeDynamicArray::insert(uint64_t i, uint64_t value) {
+    if (i >= _size) throw std::out_of_range("Index out of bounds.");
     uint64_t _i = i / 2;
     if (_i < border) {
         try {
@@ -123,6 +123,7 @@ void ConstantTimeDynamicArray::insert(uint64_t i, uint64_t value) {
                 breakChain(_i);
             } else {
                 A[2 * j] = A[2 * _i];
+
                 A[2 * j + 1] = A[2 * _i + 1];
                 makeChain(j, k);
                 initBlock(_i);
@@ -160,6 +161,7 @@ void ConstantTimeDynamicArray::insert(uint64_t i, uint64_t value) {
 }
 
 uint64_t ConstantTimeDynamicArray::get(uint64_t i) const {
+    if (i >= _size) throw std::out_of_range("Index out of bounds.");
     uint64_t _i = i / 2;
     if (isChain(_i)) {
         if (i < 2 * border) {
@@ -208,9 +210,9 @@ uint64_t ConstantTimeDynamicArray::extend() {
     uint64_t k;
     try {
         k = chainWith(border);
+        A[2 * border] = A[2 * k + 1];
+        breakChain(border);
         border++;
-        A[2 * (border - 1)] = A[2 * k + 1];
-        breakChain(border - 1);
     } catch (NotChained& e) {
         k = border++;
     }
@@ -222,7 +224,7 @@ uint64_t ConstantTimeDynamicArray::extend() {
 bool ConstantTimeDynamicArray::isChain(uint64_t i) const {
     uint64_t _k = A[2 * i];
     uint64_t k = _k / 2;
-    return (_k % 2 == 0 && _k >= 0 && _k < _size && A[_k] == 2 * i &&
+    return (_k % 2 == 0 && _k < _sizeA && A[_k] == 2 * i &&
             ((i < border && border <= k) || (k < border && border <= i)));
 }
 }  // namespace Sealib
