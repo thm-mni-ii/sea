83844e4f5377a3e6827c525f209b275b59a89801
Johannes Meintrup <johannes.meintrup@mni.thm.de>
2020-09-10 11:31:05 +0200

    add vertex induced push test

--- test/subgraphstack_test.cpp
+++ test/subgraphstack_test.cpp
@@ -146,3 +146,62 @@ TEST(SubGraphStackTest, orderDegree) {
     }
     ASSERT_EQ(stack.size(), 1);
 }
+
+/**
+for (uint64_t i = 0; i < 5; i++) {
+        bitset_t a(stack.order(i));
+        for (uint64_t j = 0; j < a.size(); j++) {
+            a[j] = j % 4 != 0;
+        }
+        stack.push_vertex_induced(a);
+    }*/
+
+TEST(SubGraphStackTest, vertexInduced) {
+    typedef Sealib::Bitset<uint64_t> bitset_t;
+
+    uint64_t order = 9;
+    auto **adj_mtrx = reinterpret_cast<uint64_t **>(malloc(sizeof(uint64_t) * order * order));
+    adj_mtrx[0] = new uint64_t[order]{0, 0, 0, 1, 1, 1, 0, 1, 0};
+    adj_mtrx[1] = new uint64_t[order]{0, 0, 0, 1, 0, 0, 0, 1, 1};
+    adj_mtrx[2] = new uint64_t[order]{0, 0, 0, 0, 1, 1, 1, 1, 0};
+    adj_mtrx[3] = new uint64_t[order]{1, 1, 0, 0, 0, 0, 0, 0, 0};
+    adj_mtrx[4] = new uint64_t[order]{1, 0, 1, 0, 0, 1, 0, 0, 0};
+    adj_mtrx[5] = new uint64_t[order]{1, 0, 1, 0, 1, 0, 0, 0, 1};
+    adj_mtrx[6] = new uint64_t[order]{0, 0, 1, 0, 0, 0, 0, 0, 0};
+    adj_mtrx[7] = new uint64_t[order]{1, 1, 1, 0, 0, 0, 0, 0, 0};
+    adj_mtrx[8] = new uint64_t[order]{0, 1, 0, 0, 0, 1, 0, 0, 0};
+
+    shared_ptr<UndirectedGraph> bg =
+        GraphCreator::createSharedPointerFromAdjacencyMatrix(adj_mtrx, order);
+
+    SubGraphStack stack(bg);
+
+    ASSERT_EQ(stack.order(), 9);
+    ASSERT_EQ(stack.degree(1), 4);
+    ASSERT_EQ(stack.degree(2), 3);
+    ASSERT_EQ(stack.degree(3), 4);
+    ASSERT_EQ(stack.degree(4), 2);
+    ASSERT_EQ(stack.degree(5), 3);
+    ASSERT_EQ(stack.degree(6), 4);
+    ASSERT_EQ(stack.degree(7), 1);
+    ASSERT_EQ(stack.degree(8), 3);
+    ASSERT_EQ(stack.degree(9), 2);
+
+    ASSERT_EQ(stack.gMax(), 26);
+
+    for (uint64_t i = 0; i < 3; i++) {
+        bitset_t a(stack.order());
+        size_t new_order = 0;
+        for (uint64_t j = 0; j < a.size(); j++) {
+            if( j % 4 != 0) {
+                a[j] = true;
+                new_order++;
+            } else {
+                a[j] = false;
+            }
+        }
+        stack.push_vertex_induced(a);
+        ASSERT_EQ(stack.size(), i+2);
+        ASSERT_EQ(stack.order(), new_order);
+    }
+}
